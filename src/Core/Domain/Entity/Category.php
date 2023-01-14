fd452fde7ec56e7926cf724ddc6be9f343dfa2ff
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-14 11:56:23 -0300

    feat: create method to update category

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -31,4 +31,9 @@ class Category
   {
     $this->isActive = false;
   }
+  public function update(string $name, string $description = ''): void
+  {
+    $this->name = $name;
+    $this->description = $description;
+  }
 }
