fd7f22d425743b8b6a1d69856eb45d7737d9a069
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 15:34:23 -0300

    feat: create method to validate min length

--- src/Core/Domain/Validation/DomainValidation.php
+++ src/Core/Domain/Validation/DomainValidation.php
@@ -16,8 +16,15 @@ class DomainValidation
 
   public static function strMaxLength(string $value, int $length = 255, string $exceptMessage = null)
   {
-    if (strlen($value) <= $length) {
+    if (strlen($value) >= $length) {
       throw new EntityValidationException($exceptMessage ?? 'The value must not be greater than {$length} characters');
+    }
+  }
+
+  public static function strMinLength(string $value, int $length = 2, string $exceptMessage = null)
+  {
+    if (strlen($value) < $length) {
+      throw new EntityValidationException($exceptMessage ?? 'The value must not be smaller than {$length} characters');
     }
   }
 }
