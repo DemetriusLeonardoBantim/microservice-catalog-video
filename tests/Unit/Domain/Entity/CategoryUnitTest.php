b2dff6b07f5ce9f76333c69a181385c900c22e24
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 17:44:55 -0300

    refactor: add id in tests

--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -12,12 +12,13 @@ class CategoryUnitTest extends TestCase
   public function testAttributes()
   {
     $category = new Category(
-      id: 'asdf',
       name: 'New Cat',
       description: 'New desc',
       isActive: true
     );
 
+
+    $this->assertNotEmpty($category->id());
     $this->assertEquals('New Cat', $category->name);
     $this->assertEquals('New desc', $category->description);
     $this->assertEquals(true, $category->isActive);
