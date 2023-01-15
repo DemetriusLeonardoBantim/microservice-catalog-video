567489f7bf3cab0d0179c36bcd8e04d12fdd488e
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 17:04:34 -0300

    feat: create class uuid

---
+++ src/Core/Domain/ValueObject/Uuid.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Domain\ValueObject;
+
+use Exception;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+class Uuid
+{
+  public function __construct(protected string $value)
+  {
+    $this->ensureIsValid($value);
+  }
+
+  public static function random(): self
+  {
+    return new  self(RamseyUuid::uuid4()->toString());
+  }
+
+  private function ensureIsValid(string $id)
+  {
+    if (!RamseyUuid::isValid($id)) {
+      throw new InvalidArgumentException(sprintf('<%s>does not allow the value<%s>', static::class, $id));
+    }
+  }
+}
