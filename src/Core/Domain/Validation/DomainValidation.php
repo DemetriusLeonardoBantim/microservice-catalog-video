12b8dd4b4a3bee8a4c28d938698faf9477135393
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 15:26:29 -0300

    feat: create method to validate max length

--- src/Core/Domain/Validation/DomainValidation.php
+++ src/Core/Domain/Validation/DomainValidation.php
@@ -13,4 +13,11 @@ class DomainValidation
       throw new EntityValidationException($exceptMessage ?? 'Should not empty');
     }
   }
+
+  public static function strMaxLength(string $value, int $length = 255, string $exceptMessage = null)
+  {
+    if (strlen($value) <= $length) {
+      throw new EntityValidationException($exceptMessage ?? 'The value must not be greater than {$length} characters');
+    }
+  }
 }
