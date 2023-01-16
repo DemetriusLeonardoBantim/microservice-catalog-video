5d064dc31425b8b6ad1aa2e910f866b3359c5db7
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 22:31:06 -0300

    feat: create findAll in repository

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -7,4 +7,5 @@ use Core\Domain\Entity\Category;
 interface CategoryRepositoryInterface
 {
   public function insert(Category $category): Category;
+  public function findAll(string $filter = '', $order = 'DESC');
 }
