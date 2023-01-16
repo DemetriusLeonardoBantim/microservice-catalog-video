b523a3b777507261e8e208a1723464b4059e59e7
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-16 13:13:51 -0300

    feat: create CreateUseCaseCategoryUnitTest

---
+++ tests/Unit/UseCase/Category/CreateCategoryUseCaseUnit.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit\UseCase\Category;
+
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\CreateCategoryUseCase\CreateCategoryUseCase;
+use PHPUnit\Framework\TestCase;
+use Mockery;
+
+class CreateUseCaseCategoryUnitTest extends TestCase
+{
+  public function testCreateNewCategory()
+  {
+    $categoryId = '1';
+    $categoryName = 'name cat';
+
+    $mockEntity = Mockery::mock(Category::class, [
+      $categoryId,
+      $categoryName
+    ]);
+    $mockRepo = Mockery::mock(stdClass::class, CreateCategoryUseCase::class);
+    $mockRepo->shouldReceive('insert')->andReturn();
+    $useCase = new CreateCategoryUseCase($this->$mockRepo);
+    $useCase->execute();
+  }
+}
