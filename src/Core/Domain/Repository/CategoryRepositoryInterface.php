5dd8bb94c7c1c4ddf2e95088b8655391db33698c
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 22:32:46 -0300

    feat: create paginate in CategoryRepository

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -7,5 +7,6 @@ use Core\Domain\Entity\Category;
 interface CategoryRepositoryInterface
 {
   public function insert(Category $category): Category;
-  public function findAll(string $filter = '', $order = 'DESC');
+  public function findAll(string $filter = '', $order = 'DESC'): array;
+  public function paginate(string $filter = '', $order = 'DESC', int $page, int $totalPage = 15): array;
 }
