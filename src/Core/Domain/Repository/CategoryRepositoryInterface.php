6cd4a8caba9c78828e561a983f4707cbc1653a5a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-15 22:34:48 -0300

    feat: create method findById in CategoryRepository

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -7,6 +7,7 @@ use Core\Domain\Entity\Category;
 interface CategoryRepositoryInterface
 {
   public function insert(Category $category): Category;
+  public function findById(string $id): Category;
   public function findAll(string $filter = '', $order = 'DESC'): array;
   public function paginate(string $filter = '', $order = 'DESC', int $page, int $totalPage = 15): array;
 }
