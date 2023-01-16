5c88e4183a30775b506eed5ac8d4bb914bfbe30c
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-16 13:17:18 -0300

    feat: create exeucte method in CreateCategoryUseCase

--- src/UseCase/Category/CreateCategoryUseCase.php
+++ src/UseCase/Category/CreateCategoryUseCase.php
@@ -2,6 +2,13 @@
 
 namespace Core\UseCase\CreateCategoryUseCase;
 
+use Core\Domain\Repository\CategoryRepositoryInterface;
+
 class CreateCategoryUseCase
 {
+  protected $repository;
+  public function __counstruct(CategoryRepositoryInterface $repository)
+  {
+    $this->repository = $repository;
+  }
 }
