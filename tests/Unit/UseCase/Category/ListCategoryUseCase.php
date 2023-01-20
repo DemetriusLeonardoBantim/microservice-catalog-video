e9387fc0a58bd5bbe1ecce1268b91bf58a4c1405
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-20 14:27:15 -0300

    refactor: use correct DTO in tests

--- tests/Unit/UseCase/Category/ListCategoryUseCase.php
+++ tests/Unit/UseCase/Category/ListCategoryUseCase.php
@@ -6,11 +6,15 @@ use Rasmery\Uuid\Uuid;
 use Core\Domain\Entity\Category as CategoryEntity;
 use Core\Domain\Repository\CategoryRepositoryInterface;
 use Core\UseCase\CreateCategoryUseCase\CreateCategoryUseCase;
+use Core\UseCase\CreateCategoryUseCase\ListCategoryUseCase;
 use Core\UseCase\DTO\Category\CategoryCreateInputDTO;
 use Core\UseCase\DTO\Category\CategoryCreateOutputDTO;
 use PHPUnit\Framework\TestCase;
 use Mockery;
-
+use Core\UseCase\DTO\Category\{
+  CategoryInputDto,
+  CategoryOutputDto
+};
 
 class ListCategoryUseCaseUnitTests extends TestCase
 {
@@ -25,14 +29,14 @@ class ListCategoryUseCaseUnitTests extends TestCase
     $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
     $this->mockRepo->shouldReceive('findById')->with($uuid)->andReturn($this->mockEntity);
 
-    $this->mockInputDto = Mockery::mock(CategoryCreateInputDTO::class, [
+    $this->mockInputDto = Mockery::mock(CategoryInputDto::class, [
       $uuid,
     ]);
 
     $useCase = new ListCategoryUseCase($this->mockRepo);
     $response = $useCase->execute($this->mockInputDTO);
 
-    $this->assertInstanceOf(CategoryOutupDto::class, $response);
+    $this->assertInstanceOf(CategoryOutputDto::class, $response);
     $this->assertEquals('test category', $response->name);
     $this->assertEquals($uuid, $response->id);
   }
