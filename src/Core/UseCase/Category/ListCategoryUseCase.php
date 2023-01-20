c607fc1500664ed6259d4f6d31f6543787f77c4a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2023-01-20 14:25:45 -0300

    feat: create method execute to list category

--- src/Core/UseCase/Category/ListCategoryUseCase.php
+++ src/Core/UseCase/Category/ListCategoryUseCase.php
@@ -4,7 +4,10 @@ namespace Core\UseCase\CreateCategoryUseCase;
 
 use Core\Domain\Entity\Category;
 use Core\Domain\Repository\CategoryRepositoryInterface;
-
+use Core\UseCase\DTO\Category\{
+  CategoryInputDto,
+  CategoryOutputDto
+};
 
 
 class ListCategoryUseCase
@@ -13,5 +16,16 @@ class ListCategoryUseCase
   public function __counstruct(CategoryRepositoryInterface $repository)
   {
     $this->repository = $repository;
+  }
+
+  public function execute(CategoryInputDto $input): CategoryOutputDto
+  {
+    $category = $this->repository->findById($input->id);
+    return new CategoryOutputDto(
+      id: $category->id(),
+      name: $category->name,
+      description: $category->description,
+      is_active: $category->isActive,
+    );
   }
 }
